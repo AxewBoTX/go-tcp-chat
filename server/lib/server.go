9ae974f4d37dc55a76080ac76445b17747632dda
AxewBoTX <lovedeepbrar54155@gmail.com>
2024-02-05 21:37:54 +0530

    server base file

---
+++ server/lib/server.go
@@ -0,0 +1,46 @@
+package lib
+
+import (
+	"log"
+	"net"
+
+	"github.com/fatih/color"
+)
+
+type Server struct {
+	listener net.Listener
+}
+
+func NewServer() *Server {
+	listener, listener_err := net.Listen("tcp", SERVER_ADDR)
+	if listener_err != nil {
+		color.Set(color.FgRed)
+		log.Println("Server Listener Error:", listener_err)
+		color.Unset()
+	}
+	return &Server{listener: listener}
+}
+
+func (server *Server) Start() {
+	color.Set(color.FgGreen)
+	log.Println("Server running on", "http://"+SERVER_ADDR)
+	color.Unset()
+
+	defer func() {
+		server.listener.Close()
+		color.Set(color.FgYellow)
+		log.Println("Server closing on", "https://"+SERVER_ADDR)
+		color.Unset()
+	}()
+
+	for {
+		conn, conn_err := server.listener.Accept()
+		if conn_err != nil {
+			color.Set(color.FgRed)
+			log.Println("Conneciton Accetp Error:", conn_err)
+			color.Unset()
+		}
+
+		go server.HandleConnection(conn)
+	}
+}
