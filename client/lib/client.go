f5aa62ffddd92703a5a8cbee4904f6d8010ecde0
AxewBoTX <lovedeepbrar54155@gmail.com>
2024-02-06 18:20:42 +0530

    Client struct && ReadLoop() && WriteLoop()
    creating a seprate struct for the client &&
    implementing a ReadLoop() for reading data from the server &&
    implementing a WriteLoop() for writing data to the server

--- client/lib/client.go
+++ client/lib/client.go
@@ -1,1 +1,54 @@
 package lib
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"github.com/fatih/color"
+)
+
+type (
+	Client struct {
+		Conn net.Conn
+	}
+)
+
+func NewClient() *Client {
+	conn, conn_err := net.Dial("tcp", SERVER_ADDR)
+	if conn_err != nil {
+		color.Set(color.FgRed)
+		log.Fatal("TCP Connection Error:", conn_err)
+		color.Unset()
+	}
+	return &Client{Conn: conn}
+}
+
+func (client *Client) ReadLoop() {
+	go func() { // Running in a goroutine
+		for {
+			read_buff := make([]byte, 2048)
+			if _, read_err := client.Conn.Read(read_buff); read_err != nil {
+				color.Set(color.FgRed)
+				log.Fatal("Server Recieve Error:", read_err)
+				color.Unset()
+				return
+			}
+			fmt.Printf("-> %s\n", read_buff)
+		}
+	}()
+}
+
+func (client *Client) WriteLoop() {
+	user_input_scanner := bufio.NewScanner(os.Stdin)
+	for user_input_scanner.Scan() {
+		if _, send_err := client.Conn.Write([]byte(user_input_scanner.Text() + "\n")); send_err != nil {
+			color.Set(color.FgRed)
+			log.Println("User Input Read Error:", send_err)
+			color.Unset()
+			return
+		}
+	}
+}
