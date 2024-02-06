4d24e6953a1910484dc2ea4fe6bb330c97217686
AxewBoTX <lovedeepbrar54155@gmail.com>
2024-02-06 11:30:58 +0530

    comments, var grouping, handle client connections, message handling, message broadcasting
    adding comments to various parts of the server code &&
    grouping vars, types and constants together into on keyword for better visuals &&
    handling client connection; taking client input and send that input to specific message channel &&
    handling message input from clients in goroutine &&
    broadcast message to all the clients

--- server/lib/lib.go
+++ server/lib/lib.go
@@ -1,25 +1,92 @@
 package lib
 
 import (
+	"bufio"
+	"fmt"
+	"log"
 	"net"
 
 	"github.com/fatih/color"
 )
 
-const (
+const ( // Constants
 	SERVER_ADDR string = "localhost:8080"
 )
 
-type Client struct {
-}
+type ( // Objects
+	Client struct {
+		Conn net.Conn `json:"conn"`
+	}
+	Message struct {
+		Client_ADDR string `json:"client_addr"`
+		Body        string `json:"body"`
+	}
+)
 
+var ( // Variables
+	Clients        = make(map[string]Client)
+	JOIN_Messages  = make(chan Message)
+	Messages       = make(chan Message)
+	LEAVE_Messages = make(chan Message)
+)
+
+// Handle individual connections
 func (server *Server) HandleConnection(conn net.Conn) {
-	color.Green(conn.RemoteAddr().String() + " joined the server!\n")
-	defer func() {
-		color.Yellow(conn.RemoteAddr().String() + " left the server!\n")
-		conn.Close()
+	client_addr := conn.RemoteAddr().String()
+	Clients[client_addr] = Client{Conn: conn}
+
+	JOIN_Messages <- Message{Client_ADDR: client_addr, Body: client_addr + " joined the server"}
+
+	go func() {
+		client_scanner := bufio.NewScanner(conn)
+		for client_scanner.Scan() {
+			Messages <- Message{Client_ADDR: client_addr, Body: client_scanner.Text()}
+		}
+		defer func() {
+			LEAVE_Messages <- Message{Client_ADDR: client_addr, Body: client_addr + " left the server."}
+			delete(Clients, client_addr)
+			conn.Close()
+		}()
 	}()
 }
 
+// Manage Connections
 func (server *Server) ManageConnections() {
+	for {
+		select {
+		case data := <-JOIN_Messages: // Client Join Message
+			color.Set(color.FgGreen)
+			for _, client := range Clients {
+				if client.Conn.RemoteAddr().String() != data.Client_ADDR {
+					if _, broadcast_err := client.Conn.Write([]byte(data.Client_ADDR + ": " + data.Body)); broadcast_err != nil {
+						log.Println(data.Body)
+					}
+				}
+			}
+			log.Println(data.Body)
+			color.Unset()
+		case data := <-Messages: // Broadcaster
+			for _, client := range Clients {
+				if client.Conn.RemoteAddr().String() != data.Client_ADDR {
+					if _, broadcast_err := client.Conn.Write([]byte(data.Client_ADDR + ": " + data.Body)); broadcast_err != nil {
+						color.Set(color.FgRed)
+						log.Println(data.Body)
+						color.Unset()
+					}
+				}
+			}
+			fmt.Printf("-> %s\n", data.Client_ADDR+": "+data.Body)
+		case data := <-LEAVE_Messages: // Client Leave Message
+			color.Set(color.FgYellow)
+			for _, client := range Clients {
+				if client.Conn.RemoteAddr().String() != data.Client_ADDR {
+					if _, broadcast_err := client.Conn.Write([]byte(data.Client_ADDR + ": " + data.Body)); broadcast_err != nil {
+						log.Println(data.Body)
+					}
+				}
+			}
+			log.Println(data.Body)
+			color.Unset()
+		}
+	}
 }
