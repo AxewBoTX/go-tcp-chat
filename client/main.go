87c609e441f21b52054acecec0b5a8ced4c91b91
AxewBoTX <lovedeepbrar54155@gmail.com>
2024-02-07 16:23:15 +0530

    username before connection
    adding the functionality to require the username of the client before establishin the connection to the server

--- client/main.go
+++ client/main.go
@@ -1,11 +1,36 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/fatih/color"
+
 	"client/lib"
 )
 
 func main() {
+	// Getting The Username
+	username_reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter your username: ")
+	username, username_read_err := username_reader.ReadString('\n')
+	username = strings.TrimSpace(username)
+	if username_read_err != nil {
+		color.Set(color.FgRed)
+		log.Fatal("Username Read Error:", username_read_err)
+		color.Unset()
+	}
+	if len(username) == 0 || username == "" {
+		color.Set(color.FgRed)
+		log.Fatal("Username is required!")
+		color.Unset()
+	}
+
 	client := lib.NewClient()
+	client.Username = username
 
 	client.ReadLoop()
 	client.WriteLoop()
