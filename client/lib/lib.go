dc2abcd7cc550e96f36505bf05646b6c77ebd920
AxewBoTX <lovedeepbrar54155@gmail.com>
2024-02-07 16:23:30 +0530

    Client, Message struct

--- client/lib/lib.go
+++ client/lib/lib.go
@@ -1,5 +1,21 @@
 package lib
 
+import (
+	"net"
+)
+
 const ( // Constants
 	SERVER_ADDR string = "localhost:8080"
+)
+
+type ( // Objects
+	Client struct {
+		Conn     net.Conn
+		Username string `json:"username"`
+	}
+	Message struct {
+		Client Client `json:"client"`
+		Method string `json:"method"`
+		Body   string `json:"body"`
+	}
 )
