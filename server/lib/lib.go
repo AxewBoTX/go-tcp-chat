8d47514ddb96ea13dda8bbdca37b35b1120780f9
AxewBoTX <lovedeepbrar54155@gmail.com>
2024-02-07 12:17:03 +0530

    sorting functions
    moving the handleConnection && manageConnection functions to the /server/lib/server.go file &&
    creating a NewMessage() and NewClient() for creating structs

--- server/lib/lib.go
+++ server/lib/lib.go
@@ -27,63 +27,10 @@ var ( // Variables
 	LEAVE_Messages = make(chan Message)
 )
 
-// Handle individual connections
-func (server *Server) HandleConnection(conn net.Conn) {
-	client_addr := conn.RemoteAddr().String()
-	Clients[client_addr] = Client{Conn: conn}
-
-	JOIN_Messages <- Message{Client_ADDR: client_addr, Body: client_addr + " joined the server"}
-
-	go func() {
-		client_scanner := bufio.NewScanner(conn)
-		for client_scanner.Scan() {
-			Messages <- Message{Client_ADDR: client_addr, Body: client_scanner.Text()}
-		}
-		defer func() {
-			LEAVE_Messages <- Message{Client_ADDR: client_addr, Body: client_addr + " left the server."}
-			delete(Clients, client_addr)
-			conn.Close()
-		}()
-	}()
+func NewClient(conn net.Conn, username string) *Client {
+	return &Client{Conn: conn, Username: username}
 }
 
-// Manage Connections
-func (server *Server) ManageConnections() {
-	for {
-		select {
-		case data := <-JOIN_Messages: // Client Join Message
-			color.Set(color.FgGreen)
-			for _, client := range Clients {
-				if client.Conn.RemoteAddr().String() != data.Client_ADDR {
-					if _, broadcast_err := client.Conn.Write([]byte(data.Client_ADDR + ": " + data.Body)); broadcast_err != nil {
-						log.Println(data.Body)
-					}
-				}
-			}
-			log.Println(data.Body)
-			color.Unset()
-		case data := <-Messages: // Broadcaster
-			for _, client := range Clients {
-				if client.Conn.RemoteAddr().String() != data.Client_ADDR {
-					if _, broadcast_err := client.Conn.Write([]byte(data.Client_ADDR + ": " + data.Body)); broadcast_err != nil {
-						color.Set(color.FgRed)
-						log.Println(data.Body)
-						color.Unset()
-					}
-				}
-			}
-			fmt.Printf("-> %s\n", data.Client_ADDR+": "+data.Body)
-		case data := <-LEAVE_Messages: // Client Leave Message
-			color.Set(color.FgYellow)
-			for _, client := range Clients {
-				if client.Conn.RemoteAddr().String() != data.Client_ADDR {
-					if _, broadcast_err := client.Conn.Write([]byte(data.Client_ADDR + ": " + data.Body)); broadcast_err != nil {
-						log.Println(data.Body)
-					}
-				}
-			}
-			log.Println(data.Body)
-			color.Unset()
-		}
-	}
+func NewMessage(client Client, method string, body string) *Message {
+	return &Message{Client: client, Method: method, Body: body}
 }
