4a8e3615aa3f2375263d6a4b2972e84ed17c7b73
AxewBoTX <lovedeepbrar54155@gmail.com>
2024-02-07 12:25:42 +0530

    improve ManageConnections()
    broadcast JOIN,MSG,LEAVE type messages to all the clients efficiently

--- server/lib/server.go
+++ server/lib/server.go
@@ -46,3 +46,46 @@ func (server *Server) Start() {
 		go server.HandleConnection(conn)
 	}
 }
+func (server *Server) ManageConnections() {
+	for {
+		select {
+		case data := <-JOIN_Messages: // Client Join Message
+			for _, client := range Clients {
+				if client.Conn.RemoteAddr().String() != data.Client.Conn.RemoteAddr().String() {
+					if _, join_message_err := client.Conn.Write([]byte(data.Client.Username + " joined the server!")); join_message_err != nil {
+						color.Set(color.FgRed)
+						log.Println("JOIN Message Error:", join_message_err)
+						color.Unset()
+					}
+				}
+			}
+			color.Set(color.FgGreen)
+			log.Println(data.Client.Username + " joined the server!")
+			color.Unset()
+		case data := <-Messages: // Message Broadcaster
+			for _, client := range Clients {
+				if client.Conn.RemoteAddr().String() != data.Client.Conn.RemoteAddr().String() {
+					if _, message_broadcast_err := client.Conn.Write([]byte(data.Client.Username + ": " + data.Body)); message_broadcast_err != nil {
+						color.Set(color.FgRed)
+						log.Println("Message Broadcast Error:", message_broadcast_err)
+						color.Unset()
+					}
+				}
+			}
+			log.Println(data.Client.Username + ": " + data.Body)
+		case data := <-LEAVE_Messages: // Client Leave Message
+			for _, client := range Clients {
+				if client.Conn.RemoteAddr().String() != data.Client.Conn.RemoteAddr().String() {
+					if _, leave_message_err := client.Conn.Write([]byte(data.Client.Username + " left the server!")); leave_message_err != nil {
+						color.Set(color.FgRed)
+						log.Println(leave_message_err)
+						color.Unset()
+					}
+				}
+			}
+			color.Set(color.FgYellow)
+			log.Println(data.Client.Username + " left the server!")
+			color.Unset()
+		}
+	}
+}
