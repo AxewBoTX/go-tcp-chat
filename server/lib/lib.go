5e8f5ee634d1ce376be3ecf78bf1b92b7d6d2701
AxewBoTX <lovedeepbrar54155@gmail.com>
2024-02-08 12:09:00 +0530

    seprate message broadcaster, message logger
    better, clean and less code for broadcasting and printing message on the server &&
    use the GenerateMsgBody() function for reusability &&
    use a LogMsg() for logging reusability and message of specific methods to the server console

--- server/lib/lib.go
+++ server/lib/lib.go
@@ -1,7 +1,11 @@
 package lib
 
 import (
+	"encoding/json"
+	"log"
 	"net"
+
+	"github.com/fatih/color"
 )
 
 const ( // Constants
@@ -33,4 +37,47 @@ func NewClient(conn net.Conn, username string) *Client {
 
 func NewMessage(client Client, method string, body string) *Message {
 	return &Message{Client: client, Method: method, Body: body}
+}
+
+// broadcast message to the network
+func BroadcastMessage(msg Message) {
+	for _, client := range Clients {
+		if client.Conn.RemoteAddr().String() != msg.Client.Conn.RemoteAddr().String() {
+			msg_broadcast_encoder := json.NewEncoder(client.Conn)
+			if message_broadcast_err := msg_broadcast_encoder.Encode(msg); message_broadcast_err != nil {
+				color.Set(color.FgRed)
+				log.Println("Message Broadcast Error:", message_broadcast_err)
+				color.Unset()
+			}
+		}
+	}
+	LogMsg(msg)
+}
+
+// generating body for message
+func GenerateMsgBody(msg Message) string {
+	if msg.Method == "JOIN" {
+		return msg.Client.Username + " joined the server!"
+	} else if msg.Method == "MSG" {
+		return msg.Client.Username + ": " + msg.Body
+	} else if msg.Method == "LEAVE" {
+		return msg.Client.Username + " left the server!"
+	} else {
+		return ""
+	}
+}
+
+// logging message to the server
+func LogMsg(msg Message) {
+	if msg.Method == "JOIN" {
+		color.Set(color.FgGreen)
+		log.Println(msg.Client.Username + " joined the server!")
+		color.Unset()
+	} else if msg.Method == "MSG" {
+		log.Println(msg.Client.Username + ": " + msg.Body)
+	} else if msg.Method == "LEAVE" {
+		color.Set(color.FgYellow)
+		log.Println(msg.Client.Username + " left the server!")
+		color.Unset()
+	}
 }
