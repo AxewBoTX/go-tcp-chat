12651e02fef71f6a8c1a74474002b80772f87056
AxewBoTX <lovedeepbrar54155@gmail.com>
2024-02-08 12:05:52 +0530

    print message to client
    print the message body to the client console by using the Message struct

--- client/lib/lib.go
+++ client/lib/lib.go
@@ -1,7 +1,10 @@
 package lib
 
 import (
+	"fmt"
 	"net"
+
+	"github.com/fatih/color"
 )
 
 const ( // Constants
@@ -19,3 +22,17 @@ type ( // Objects
 		Body   string `json:"body"`
 	}
 )
+
+func PrintMSG(msg Message) {
+	if msg.Method == "JOIN" {
+		color.Set(color.FgGreen)
+		fmt.Println(msg.Client.Username + " joined the server!")
+		color.Unset()
+	} else if msg.Method == "MSG" {
+		fmt.Println(msg.Client.Username + ": " + msg.Body)
+	} else if msg.Method == "LEAVE" {
+		color.Set(color.FgYellow)
+		fmt.Println(msg.Client.Username + " left the server!")
+		color.Unset()
+	}
+}
